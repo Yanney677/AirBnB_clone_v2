071d123c03d789b6631edc429160f7c95f32a391
Yanney677 <w.yanney1991@gmail.com>
2024-01-02 02:41:49 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Flash Web Application """
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Returns a Message when called """
-    return 'Hello HBNB!'
+    """ Returns Hello HBNB!"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
-
